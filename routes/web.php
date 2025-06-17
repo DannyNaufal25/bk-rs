4b6725bb50debc5759d1ff6e10ab6ec99a0e1be9
DannyNaufal25 <dannyprof2004@gmail.com>
2025-06-17 20:10:37 +0700

    refactor: remove unused pasien routes and clean up route structure

--- routes/web.php
+++ routes/web.php
@@ -26,21 +26,8 @@ Route::middleware(['auth', 'role:dokter'])->prefix('dokter')->group(function () 
     })->name('dokter.dashboard');
 });
 
-Route::middleware(['auth', 'role:pasien'])->prefix('pasien')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('pasien.dashboard');
-    })->name('pasien.dashboard');
-});
 
-Route::prefix('janjiperiksa')->group(function(){
-    Route::get('/',[JanjiPeriksaController::class,'index'])->name('pasien.janjiperiksa.index');
-    Route::post('/',[JanjiPeriksaController::class,'store'])->name('pasien.janjiperiksa.store');
-});
-  Route::prefix('riwayat-periksa')->group(function(){
-        Route::get('/', [RiwayatPeriksaController::class, 'index'])->name('pasien.riwayat-periksa.index');
-        Route::get('/{id}/detail', [RiwayatPeriksaController::class, 'detail'])->name('pasien.riwayat-periksa.detail');
-        Route::get('/{id}/riwayat', [RiwayatPeriksaController::class, 'riwayat'])->name('pasien.riwayat-periksa.riwayat');
-    });
+
 
 Route::patch('/dokter/memeriksa/{janji}/toggle-status', [\App\Http\Controllers\Dokter\MemeriksaController::class, 'toggleStatus'])->name('dokter.memeriksa.toggle-status');
 
