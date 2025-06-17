840802f0fe5f01d0dd45dbb3f488b3418cf0740f
DannyNaufal25 <dannyprof2004@gmail.com>
2025-06-17 20:10:19 +0700

    refactor: enhance route structure and enforce role-based access for pasien routes

--- routes/pasien.php
+++ routes/pasien.php
@@ -1,10 +1,21 @@
 <?php
 
+use App\Http\Controllers\Pasien\JanjiPeriksaController;
+use App\Http\Controllers\pasien\RiwayatPeriksaController;
 use Illuminate\Support\Facades\Route;
 
 // Route khusus untuk role pasien
-Route::middleware(['auth'])->group(function () {
-    Route::get('/pasien/dashboard', function () {
+Route::middleware(['auth', 'role:pasien'])->prefix('pasien')->group(function () {
+    Route::get('/dashboard', function () {
         return view('pasien.dashboard');
     })->name('pasien.dashboard');
+    Route::prefix('janjiperiksa')->group(function(){
+        Route::get('/',[JanjiPeriksaController::class,'index'])->name('pasien.janjiperiksa.index');
+        Route::post('/',[JanjiPeriksaController::class,'store'])->name('pasien.janjiperiksa.store');
+    });
+      Route::prefix('riwayat-periksa')->group(function(){
+            Route::get('/', [RiwayatPeriksaController::class, 'index'])->name('pasien.riwayat-periksa.index');
+            Route::get('/{id}/detail', [RiwayatPeriksaController::class, 'detail'])->name('pasien.riwayat-periksa.detail');
+            Route::get('/{id}/riwayat', [RiwayatPeriksaController::class, 'riwayat'])->name('pasien.riwayat-periksa.riwayat');
+        });
 });
